bfb47f1a69143ddef5a2a8dfe97449bb0afcc076
Anxela Redondo <angela.redondo@zerintia.com>
2021-12-23 12:02:13 +0100

    add init pet

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -1,16 +1,23 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Event } from '../models/event.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EventsService {
-
-  constructor(private http: HttpClient) { }
+  private headers: HttpHeaders;
+  constructor(private http: HttpClient) {
+    this.headers = new HttpHeaders();
+  }
 
   getAllEvents(): Observable<Event[]> {
-    return this.http.get<Event[]>('https://www.cultura.gal/v1/axenda/eventos.json?idioma=gl&concello=270')
+    this.http
+      .get<string>('https://www.cultura.gal/v1/axenda/')
+      .subscribe((res) => console.info(res));
+    return this.http.get<Event[]>(
+      'https://www.cultura.gal/v1/axenda/eventos.json?idioma=gl&concello=270'
+    );
   }
 }
