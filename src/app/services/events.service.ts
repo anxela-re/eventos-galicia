d567640214d38413108c8e1a6d73708f117ab581
Anxela Redondo <angela.redondo@zerintia.com>
2021-12-23 10:59:01 +0100

    url

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -11,6 +11,6 @@ export class EventsService {
   constructor(private http: HttpClient) { }
 
   getAllEvents(): Observable<Event[]> {
-    return this.http.get<Event[]>('https://abertos.xunta.gal/catalogo/cultura-ocio-deporte/-/dataset/0045/axenda-cultura-galicia/103/acceso-aos-datos.json')
+    return this.http.get<Event[]>('https://www.cultura.gal/v1/axenda/eventos.json?idioma=gl&concello=270')
   }
 }
